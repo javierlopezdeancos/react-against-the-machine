f98dc6ca7947622340c69697ea570649a3ee8d0c
Javier López de Ancos <javierlopezdeancos@outlook.com>
2021-12-27 13:51:03 +0100

    fix: do some adjustments

--- src/example-ratm/src/App.tsx
+++ src/example-ratm/src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
-// import the react against the machine pieces
-// import Machine, { State, Transition, Content } from 'react-against-the-machine';
-import Machine, { State, Transition, Content } from './ratm';
+import Machine, { MachineProvider, State, Transition, Content } from '../../index';
 
-// import any bus that implements the IBus interface
 import { laGuaGua as bus } from 'laguagua';
 
 import StateOne from './StateOne.component';
@@ -21,31 +18,33 @@ const goToStateThree = () => bus.publish('go::three');
 
 function App() {
   return (
-    <Machine initial="StateOne" bus={bus} logged>
-      <State id="StateOne" private={false}>
-        <Content>
-          <StateOne onGoToStateTwo={goToStateTwo} onGoToStateThree={goToStateThree} />
-        </Content>
-        <Transition event="go::two" state="StateTwo" />
-        <Transition event="go::three" state="StateThree" />
-      </State>
+    <MachineProvider>
+      <Machine initial="StateOne" bus={bus} logged={false}>
+        <State id="StateOne" private={false}>
+          <Content>
+            <StateOne onGoToStateTwo={goToStateTwo} onGoToStateThree={goToStateThree} />
+          </Content>
+          <Transition event="go::two" state="StateTwo" />
+          <Transition event="go::three" state="StateThree" />
+        </State>
 
-      <State id="StateTwo" private={false}>
-        <Content>
-          <StateTwo onGoToStateOne={goToStateOne} onGoToStateThree={goToStateThree} />
-        </Content>
-        <Transition event="go::one" state="StateOne" />
-        <Transition event="go::three" state="StateThree" />
-      </State>
+        <State id="StateTwo" private={true}>
+          <Content>
+            <StateTwo onGoToStateOne={goToStateOne} onGoToStateThree={goToStateThree} />
+          </Content>
+          <Transition event="go::one" state="StateOne" />
+          <Transition event="go::three" state="StateThree" />
+        </State>
 
-      <State id="StateThree" private={false}>
-        <Content>
-          <StateThree onGoToStateTwo={goToStateTwo} onGoToStateOne={goToStateOne} />
-        </Content>
-        <Transition event="go::one" state="StateOne" />
-        <Transition event="go::two" state="StateTwo" />
-      </State>
-    </Machine>
+        <State id="StateThree" private={false}>
+          <Content>
+            <StateThree onGoToStateTwo={goToStateTwo} onGoToStateOne={goToStateOne} />
+          </Content>
+          <Transition event="go::one" state="StateOne" />
+          <Transition event="go::two" state="StateTwo" />
+        </State>
+      </Machine>
+    </MachineProvider>
   );
 }
 
