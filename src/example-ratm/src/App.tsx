8300c904ea54d4fc28c31dcf7f33e69310ccfbd4
Javier López de Ancos <javierlopezdeancos@outlook.com>
2021-12-29 21:20:06 +0100

    feat: adapt components and app to new params state case

--- src/example-ratm/src/App.tsx
+++ src/example-ratm/src/App.tsx
@@ -1,47 +1,59 @@
-import React from 'react';
-
-import Machine, { MachineProvider, State, Transition, Content } from '../../index';
-
+import React, { useState } from 'react';
 import { laGuaGua as bus } from 'laguagua';
 
+import Machine, { MachineProvider, State, Transition, Content } from '../../index';
 import StateOne from './StateOne.component';
 import StateTwo from './StateTwo.component';
 import StateThree from './StateThree.component';
 
 import './App.css';
 
-const goToStateOne = () => bus.publish('go::one');
-
-const goToStateTwo = () => bus.publish('go::two');
-
-const goToStateThree = () => bus.publish('go::three');
-
 function App() {
+  const goToStateOne = () => bus.publish('go:to:one');
+
+  const goToStateTwo = () => bus.publish('go:to:two');
+
+  const goToStateThree = () => {
+    bus.publish('go:to:three');
+  };
+
+  const handlerEnterStateThree = (params?: Map<string, string>): void => {
+    if (!params || !params?.has) {
+      return;
+    }
+
+    const codeValue = params?.get('code');
+
+    if (codeValue) {
+      console.log('code param in URL has value: ', codeValue);
+    }
+  };
+
   return (
     <MachineProvider>
-      <Machine initial="StateOne" bus={bus} logged={false}>
+      <Machine initial="StateOne" bus={bus} logged={true}>
         <State id="StateOne" private={false}>
           <Content>
             <StateOne onGoToStateTwo={goToStateTwo} onGoToStateThree={goToStateThree} />
           </Content>
-          <Transition event="go::two" state="StateTwo" />
-          <Transition event="go::three" state="StateThree" />
+          <Transition event="go:to:two" state="StateTwo" />
+          <Transition event="go:to:three" state="StateThree" />
         </State>
 
         <State id="StateTwo" private={true}>
           <Content>
             <StateTwo onGoToStateOne={goToStateOne} onGoToStateThree={goToStateThree} />
           </Content>
-          <Transition event="go::one" state="StateOne" />
-          <Transition event="go::three" state="StateThree" />
+          <Transition event="go:to:one" state="StateOne" />
+          <Transition event="go:to:three" state="StateThree" />
         </State>
 
-        <State id="StateThree" private={false}>
+        <State id="StateThree" private={false} params={['code']} onEnter={handlerEnterStateThree}>
           <Content>
             <StateThree onGoToStateTwo={goToStateTwo} onGoToStateOne={goToStateOne} />
           </Content>
-          <Transition event="go::one" state="StateOne" />
-          <Transition event="go::two" state="StateTwo" />
+          <Transition event="go:to:one" state="StateOne" />
+          <Transition event="go:to:two" state="StateTwo" />
         </State>
       </Machine>
     </MachineProvider>
--- src/example-ratm/src/StateThree.component.tsx
+++ src/example-ratm/src/StateThree.component.tsx
@@ -1,4 +1,6 @@
-import * as React from 'react';
+import React, { useMemo } from 'react';
+
+import useMachine from '../../context';
 
 export interface IStateOneProps {
   onGoToStateTwo: () => void;
@@ -8,10 +10,21 @@ export interface IStateOneProps {
 function StateThree(props: IStateOneProps) {
   const { onGoToStateTwo, onGoToStateOne } = props;
 
+  const machine = useMachine();
+  const state = machine.getState('StateThree');
+
+  const code = useMemo(() => {
+    if (state && state?.params) {
+      const { params } = state;
+      return params?.get('code');
+    }
+  }, [state]);
+
   return (
     <div className="state-three">
       <header>
         <h1>State 3</h1>
+        {code && <h2>Your code param value in url is {code}</h2>}
       </header>
       <main>
         <button onClick={onGoToStateOne}>Go to state 1</button>
