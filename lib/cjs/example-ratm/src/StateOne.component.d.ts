374ddf6f0fa7166c4128da86eb6162a3489cb342
Javier López de Ancos <javierlopezdeancos@outlook.com>
2021-12-29 21:30:05 +0100

    build: v0.2.0

--- lib/cjs/example-ratm/src/StateOne.component.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export interface IStateOneProps {
-    onGoToStateTwo: () => void;
-    onGoToStateThree: () => void;
-}
-declare function StateOne(props: IStateOneProps): JSX.Element;
-export default StateOne;
--- lib/cjs/example-ratm/src/StateTwo.component.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export interface IStateOneProps {
-    onGoToStateOne: () => void;
-    onGoToStateThree: () => void;
-}
-declare function StateTwo(props: IStateOneProps): JSX.Element;
-export default StateTwo;
