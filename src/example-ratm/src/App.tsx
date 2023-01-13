fd056d77c900dc45e2cae42a37bf8d243eb01d8f
Javier Lopez de Ancos <javier.lopezdeancos@wayflyer.com>
2023-01-13 20:48:41 +0100

    fix: remove not needed useState

--- src/example-ratm/src/App.tsx
+++ src/example-ratm/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { laGuaGua as bus } from 'laguagua';
 
 import Machine, { MachineProvider, State, Transition, Content } from '../../index';
